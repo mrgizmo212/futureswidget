079029535b70e17f06c2a7c6a99b66c72b4e8e5a
mrgizmo212 <info@truetradinggroup.com>
2024-11-14 11:17:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -368,9 +368,3 @@ app = FastAPI(
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(
-        "main:app",
-        host="0.0.0.0",
-        port=port,
-        log_level="info"
-    )
